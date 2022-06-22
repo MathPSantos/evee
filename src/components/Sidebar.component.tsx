7aa09e49867d64ac425bdf5fdd7a6768176e5f3c
Matheus Santos <matheus.psantos2016@gmail.com>
2022-06-21 22:00:24 -0300

    feat: finilize sidebar

--- src/components/Sidebar.component.tsx
+++ src/components/Sidebar.component.tsx
@@ -1,3 +1,48 @@
+import { gql, useQuery } from "@apollo/client";
+import { Lesson } from "./Lesson.component";
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+      slug
+      title
+      availableAt
+      id
+      lessonType
+    }
+  }
+`;
+
+interface GetLessonsQueryResponse {
+  lessons: {
+    id: string;
+    slug: string;
+    title: string;
+    availableAt: string;
+    lessonType: "live" | "class";
+  }[];
+}
+
 export function Sidebar() {
-  return <h1>Sidebar</h1>;
+  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY);
+
+  return (
+    <aside className="w-[348px] bg-gray-700 p-6 border-l border-l-gray-600">
+      <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+        Cronograma de aulas
+      </span>
+
+      <div className="flex flex-col gap-8">
+        {data?.lessons.map((lesson) => (
+          <Lesson
+            key={lesson.id}
+            title={lesson.title}
+            slug={lesson.slug}
+            type={lesson.lessonType}
+            availableAt={new Date(lesson.availableAt)}
+          />
+        ))}
+      </div>
+    </aside>
+  );
 }
--- src/pages/Event.page.tsx
+++ src/pages/Event.page.tsx
@@ -1,9 +1,18 @@
 import { Header } from "../components/Header.component";
+import { Sidebar } from "../components/Sidebar.component";
+import { Video } from "../components/Video.component";
 
 export function Event() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
+
+      <main className="flex flex-1">
+        <div className="flex-1">
+          <Video />
+        </div>
+        <Sidebar />
+      </main>
     </div>
   );
 }
